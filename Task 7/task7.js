4ced87cf7597b607099ba263ca7123181b0008ca
Sourabh Singh <17singh1999@gmail.com>
2021-09-04 12:50:22 +0530

    done

--- Task 7/task7.js
+++ Task 7/task7.js
@@ -1,10 +1,44 @@
   
 var a=1;
+var age;
+var adno;
+var mno;
+function v(){
+    age = prompt("Please enter your Age (Must be Greater then 10)");
+    if (isNaN(age) || age < 10 || age > 100) {
+        var text = alert("Input not valid");
+        v();
+      }
+}
+function nmo(){
+     mno = prompt("Please enter your Mobile Number");
+    if (isNaN(mno) || !Number(mno) ){
+        alert("Eneter only number");
+        nmo();
+    }
+
+}
 function register() {
-  var name = prompt("Please enter your Full Name");
-  var age = prompt("Please enter your Adhar Number");
-  var rol = prompt("Please enter your Age");
-  var faname = prompt("Please enter your Mobile Number");
+  var name = prompt("Please enter your Full Name", " ");
+   adno = prompt("Please enter your Adhar Number");
+  if (isNaN(adno) || !Number(adno)) {
+      alert("Eneter only number");
+      adno = prompt("Please enter your Adhar Number");
+
+  }
+  age = prompt("Please enter your Age (Must be Greater then 10)");
+  if (isNaN(age) || age < 10 || age > 100) {
+      alert("Enter Age in Number (Greter then 10)");
+      v();
+  } 
+  
+  mno = prompt("Please enter your Mobile Number");
+  if (isNaN(mno) || !Number(mno) ){
+    alert("Eneter only number");
+    nmo();
+}
+
+  
   var table=document.getElementById('table')
   var row = table.insertRow(a);
   var cell1 = row.insertCell(0);
@@ -13,14 +47,13 @@ function register() {
   var cell4 = row.insertCell(3);
   var cell5 = row.insertCell(4);
   cell1.innerHTML=name;
-  cell2.innerHTML=faname;
+  cell2.innerHTML=adno;
   cell3.innerHTML=age;
-  cell4.innerHTML=rol;
+  cell4.innerHTML=mno;
   cell5.innerHTML='<button id="btnEdit"  onclick=getConfirmation()>Delete </button>';
   a+=1;
 
 }
-
 
 function getConfirmation(){
     var retVal = confirm("Are you sure want to Delete ?");
