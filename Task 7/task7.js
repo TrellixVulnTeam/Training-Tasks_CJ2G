54355d5df0442ad4aa351b38df1ff3b06524fd6b
Sourabh Singh <17singh1999@gmail.com>
2021-09-04 10:50:26 +0530

    done

--- Task 7/task7.js
+++ Task 7/task7.js
@@ -11,7 +11,7 @@ function register() {
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
   var cell4 = row.insertCell(3);
-  var cell5 =  row.insertCell(4);
+  var cell5 = row.insertCell(4);
   cell1.innerHTML=name;
   cell2.innerHTML=faname;
   cell3.innerHTML=age;
@@ -20,29 +20,31 @@ function register() {
   a+=1;
 
 }
-function remove(){
-    if (a>1){
-    document.getElementById('table').deleteRow(a-1);
-    a-=1;
-    }
-}
 
-function getConfirmation()
-    {
-        var retVal = confirm("Are you sure want to Delete ?");
-        if (retVal == true)
+
+function getConfirmation(){
+    var retVal = confirm("Are you sure want to Delete ?");
+    if (retVal == true)
         {
            
             remove();
             return true;
         } 
-        else
+    else
         {
           
             return false;
         }
     }
 
+function remove(){
+        if (a>1){
+        document.getElementById('table').deleteRow(a-1);
+        a-=1;
+        }
+    }
+
+ 
 function selectText(){
   var table = document.getElementById('table');
   for (var i=0;i < table.rows.length;i++){
@@ -53,7 +55,7 @@ function selectText(){
      this.hilite = true;
     }
     else{
-     this.style.backgroundColor=this.origColor;
+        this.style.backgroundColor=this.origColor;
      this.hilite = false;
     }
      }
